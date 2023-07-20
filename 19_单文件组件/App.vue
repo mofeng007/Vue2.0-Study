53823e23203e2cad9a0f618851ade0291abaf42c
陌风 <2364506907@qq.com>
2023-07-20 18:14:59 +0800

    23/7/20

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <school></school>
+    <student></student>
+</div>
+</template>
+
+<script>
+// 引入组件
+import School from './School.vue'
+import Student from './Student.vue'
+
+export default {
+    name:'App',
+    components:{
+        School,
+        Student
+    }
+};
+</script>
---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+      <h2>学校名称：{{ studentName }}</h2>
+      <h2>学校地址：{{ address }}</h2>
+      <button @click="showName">点我提示学生名</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Student",
+    data() {
+      return {
+        studentName: "张三",
+        address: "四川",
+      };
+    },
+    methods: {
+      showName() {
+        alert(this.studentName);
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .demo {
+      background: orange;
+  }
+  </style>
