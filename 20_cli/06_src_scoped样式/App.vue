6a4ea60a5ebfce8d866e88ab8f8561a391be7043
陌风 <2364506907@qq.com>
2023-07-24 12:33:27 +0800

    23/7/24

---
+++ 20_cli/06_src_scoped样式/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <School></School>
+    <hr>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+//引入组件
+import Student from "./components/Student.vue";
+import School from "./components/School.vue";
+
+export default {
+  name: "App",
+  components: {
+    Student,
+    School,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
